9d25a12ce166efe33f05d66fbdef4448a7962723
Patty Arao <patricia_arao@dlsu.edu.ph>
2023-12-24 00:32:07 +0800

    init: tailwindcss created: -signin page todo: -signup page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,22 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts"></script>
+
+<main class="w-full min-h-screen flex items-center justify-center bg-gradient-to-br from-purple-light to-white font-sans">
+    <div class="w-[80%] h-[80vh] flex items-center justify-center bg-gray-dark-main/30 rounded-md">
+        <div class="w-[40%] h-full flex flex-col items-center justify-center gap-10">
+            <h1 class="uppercase bg-clip-text bg-gradient-to-r from-pink-main to-peach text-transparent text-7xl font-black drop-shadow-md">pop.io</h1>
+            <div class="w-1/2 flex flex-col gap-4">
+                <input type="text" placeholder="username" class="input-box focus:outline-none">
+                <input type="password" placeholder="password" class="input-box focus:outline-none">
+            </div>
+            <div class="w-1/2 flex flex-col justify-center gap-2 ">
+                <button class="w-full p-2 bg-purple-dark rounded-md text-white">Sign in</button>
+                <p class="text-xs text-right text-white">Forgot password?</p>
+            </div>
+           <p class="mt-20 font-bold">New here? <span>Create an account!</span></p>
+        </div>
+        <div class="w-[60%] h-full flex items-center justify-center bg-white">
+            <p class="">image goes here</p>
+        </div>
+        
+    </div>
+</main>
